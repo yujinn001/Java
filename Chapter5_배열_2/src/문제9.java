73b624af370c1d1fe57d095ec25962684c28e330
user <user@DESKTOP-COJ9858>
2022-10-31 17:49:23 +0900

    위치 변경

--- Chapter5_배열_2/src/문제9.java
+++ Chapter5_배열_2/src/문제9.java
@@ -7,14 +7,23 @@ public class 문제9 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {10,20,30,50,3,60,-3};
-		for(int i =0; i<arr.length; i++)
+		
+//		for(int i =0; i<arr.length; i++)
+//		{
+//			if(arr[i]==60)
+//			{
+//				System.out.println("값이 60인 곳 >> "+ i);
+//			}
+//		}
+		int i =0;
+		for( i =0; i<arr.length; i++)
 		{
 			if(arr[i]==60)
 			{
-				System.out.println("값이 60인 곳 >> "+ i);
+					break;
 			}
 		}
-		
+		System.out.println("값이 60인 곳 >> "+ i);
 	}
 
 }
