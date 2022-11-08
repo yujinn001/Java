adebe5fe4a77fe833639739641649a606f507369
user <user@DESKTOP-COJ9858>
2022-11-08 09:03:19 +0900

    1111

---
+++ Chapter6_객체지향프로그램(클래스)/src/문제7.java
@@ -0,0 +1,20 @@
+class Shop{
+	String poster;
+	String number;
+	String webSite;
+	String homePageLanguage;
+	String time;
+	String 휴무일;
+	String 주의사항;
+	String address;
+	String 교통정보;
+	String 요약문;
+}
+public class 문제7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
