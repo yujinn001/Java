f068a563820893a1c5aaa132240c5367060a2360
14rla <14rla@DESKTOP-HSVR6Q2>
2022-11-17 16:19:23 +0900

    1111

---
+++ Chapter8_예외처리_3/src/com/sist/exception2/MusicVO.java
@@ -0,0 +1,26 @@
+package com.sist.exception2;
+
+public class MusicVO {
+	private String title;
+	private String singer;
+	private String album;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	
+}
